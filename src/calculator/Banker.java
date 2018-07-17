d2050b1a4de46507a97dca6d6a5abdd60dd834b1
Distal Fractals <DistalFractals@gmail.com>
2018-07-17 11:30:17 -0400

    Added basic methods and attributes

--- src/calculator/Banker.java
+++ src/calculator/Banker.java
@@ -5,10 +5,37 @@
  */
 package calculator;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Owner
  */
 public class Banker {
-    
+
+    public static void main(String[] args) {
+        promptUser();
+    }
+
+    public static void promptUser() {
+        String savingsOption = "Savings";
+        String checkingOption = "Checking";
+        String certificateOfDepositOption = "Certificate of Deposit";
+        String[] accountOptions = {savingsOption, checkingOption, certificateOfDepositOption};
+        int accountDecision = JOptionPane.showOptionDialog(null, "What account would you like to create?", "Account Type", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, accountOptions, savingsOption);
+        System.out.println(accountDecision);
+        String initialBalance = JOptionPane.showInputDialog(null, "Enter initial balance.");
+        String interestRate = JOptionPane.showInputDialog(null, "Ener the interest rate for the account");
+        String numberOfPeriods = JOptionPane.showInputDialog(null, "How many periods should be calculated?");
+        int firstCheckNumber;
+        if (accountDecision == 1){
+            String firstCheckNumberString = JOptionPane.showInputDialog(null, "What is the first check number?");
+            firstCheckNumber = Integer.parseInt(firstCheckNumberString);
+        }
+        int lengthUntilMature;
+        if (accountDecision == 2){
+            String lengthUntilMatureString = JOptionPane.showInputDialog(null, "How many months before the account is mature?");
+            lengthUntilMature = Integer.parseInt(lengthUntilMatureString);
+        }
+    }
 }
