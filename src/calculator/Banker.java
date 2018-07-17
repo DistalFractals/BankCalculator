e868d0e79db235cf8e9567d3080a4e1fb06f20be
chrx <chrx@localhost>
2018-07-17 16:16:25 -0400

    It's almost functional, but upgraded from last version should have made this commit sooner

--- src/calculator/Banker.java
+++ src/calculator/Banker.java
@@ -17,39 +17,41 @@ import javax.swing.JOptionPane;
  */
 public class Banker {
 
-    public static List<Account> accounts = new ArrayList();
+    public static List<Account> accounts = new ArrayList<Account>();
     private static final double DEFAULT_CHECKING_FEE = 5;
 
     public static void main(String[] args) {
         promptUser();
-        displayAccounts();
+        displayAccounts(accounts);
     }
 
     public static void promptUser() {
         int makingAccounts;
+        int currentAccountNumber = 0;
         do {
             String savingsOption = "Savings";
             String checkingOption = "Checking";
             String certificateOfDepositOption = "Certificate of Deposit";
             String[] accountOptions = {savingsOption, checkingOption, certificateOfDepositOption};
             int accountDecision = JOptionPane.showOptionDialog(null, "What account would you like to create?", "Account Type", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, accountOptions, savingsOption);
-            System.out.println(accountDecision);
             String initialBalanceString = JOptionPane.showInputDialog(null, "Enter initial balance.");
             double initialBalance = Double.valueOf(initialBalanceString);
-            String interestRateString = JOptionPane.showInputDialog(null, "Ener the interest rate for the account");
-            double interestRate = Double.valueOf(interestRateString);
+            String interestRateString = JOptionPane.showInputDialog(null, "Enter the percent yearly interest rate for the account");
+            double interestRate = ((Double.valueOf(interestRateString)) / 100)/12;
             String numberOfPeriodsString = JOptionPane.showInputDialog(null, "How many periods should be calculated?");
             int numberOfPeriods = Integer.valueOf(numberOfPeriodsString);
 
             Account currentAccount;
             try {
-                currentAccount = createAccount(accountDecision, initialBalance, interestRate);
+                currentAccount = createAccount(accountDecision, currentAccountNumber, initialBalance, interestRate, numberOfPeriods);
                 accounts.add(currentAccount);
             } catch (Exception ex) {
                 Logger.getLogger(Banker.class.getName()).log(Level.SEVERE, null, ex);
                 System.out.println("The account type specified does not exist.");
             }
             makingAccounts = JOptionPane.showConfirmDialog(null, "Would you like to continue making accounts?");
+            currentAccountNumber++;
+            
         } while (makingAccounts == JOptionPane.YES_OPTION);
     }
 
@@ -57,28 +59,41 @@ public class Banker {
         System.out.println(account);
     }
 
-    private static void displayAccounts() {
+    private static void displayAccounts(List<Account> accounts) {
         accounts.forEach((account) -> {
             account.calculate();
+
         });
     }
 
-    private static Account createAccount(int accountDecision, double initialBalance, double interestRate) throws Exception {
+    private static Account createAccount(int accountDecision, int accountNumber, double initialBalance, double interestRate, int period) throws Exception {
         switch (accountDecision) {
             case 0:
-                return new Savings(initialBalance, interestRate);
+                return new Savings(accountNumber, initialBalance, interestRate, period);
             case 1:
                 int firstCheckNumber;
                 String firstCheckNumberString = JOptionPane.showInputDialog(null, "What is the first check number?");
                 firstCheckNumber = Integer.parseInt(firstCheckNumberString);
-                return new Checking(initialBalance, interestRate, firstCheckNumber, DEFAULT_CHECKING_FEE);
+                return new Checking(accountNumber, initialBalance, interestRate, period, firstCheckNumber, DEFAULT_CHECKING_FEE);
             case 2:
                 int lengthUntilMature;
                 String lengthUntilMatureString = JOptionPane.showInputDialog(null, "How many months before the account is mature?");
                 lengthUntilMature = Integer.parseInt(lengthUntilMatureString);
-                return new CertificateOfDeposit(initialBalance, interestRate, lengthUntilMature);
+                return new CertificateOfDeposit(accountNumber, initialBalance, interestRate, period, lengthUntilMature);
             default:
                 throw new Exception("Unknown account type");
         }
+    }
+
+    static void printSeparator(Account account) {
+        for (int i = 0; i < account.toString().length() - 1; i++) {
+            System.out.print("-");
+
+        }
+        System.out.println("-");
+    }
+
+    static void displayInitialization(String initializationString) {
+        System.out.println(initializationString);
     }
 }
